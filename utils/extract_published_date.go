fa9dda6d00fdaed66c2feda7cc7469a692a4704a
Umayanga <umayangag@gmail.com>
2019-09-18 14:25:32 +0530

    fix timezone issue

--- utils/extract_published_date.go
+++ utils/extract_published_date.go
@@ -9,10 +9,10 @@ import (
 return date from string or if the date layout is different return current date
  */
 func ExtractPublishedDate(layout string, timeString string) time.Time {
-	t, err := time.Parse(layout, timeString)
+	loc, _ := time.LoadLocation("Asia/Colombo")
+	t, err := time.ParseInLocation(layout, timeString,loc)
 	if err != nil {
 		fmt.Println("error in date", err)
-		loc, _ := time.LoadLocation("UTC")
 		t = time.Now().In(loc)
 	}
 	return t
